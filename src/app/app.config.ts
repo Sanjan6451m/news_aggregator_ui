2434b051d333b5ba74546dd3f91c9e3329a34be3
sanjanm <sanjan.m@softcons.net>
2025-03-11 10:59:47 +0530

    latest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
+import { withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
     provideAnimations(),
     provideClientHydration()
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, map, tap } from 'rxjs';
+import { Observable, catchError, map, tap, of } from 'rxjs';
 import { NewsArticle } from '../models/news.interface';
+import { isPlatformServer, isPlatformBrowser } from '@angular/common';
+import { StateKey, TransferState, makeStateKey } from '@angular/core';
+
+const NEWS_KEY = makeStateKey<NewsArticle[]>('news');
 
 interface NewsResponse {
   articles: NewsArticle[];
@@ -15,27 +19,55 @@ interface NewsResponse {
 })
 export class NewsService {
   private apiUrl = 'https://news-aggregator-api.vercel.app/api/news';
+  private isBrowser: boolean;
+  private platformId: Object;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(TransferState) private transferState: TransferState,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+    this.platformId = platformId;
+  }
 
   getNews(): Observable<NewsArticle[]> {
+    // First, try to get the data from transfer state
+    const existingNews = this.transferState.get(NEWS_KEY, null);
+    
+    // If we have data in transfer state and we're in the browser, use it
+    if (this.isBrowser && existingNews) {
+      return of(existingNews);
+    }
+
     console.log('Fetching news from:', this.apiUrl);
     return this.http.get<NewsResponse>(this.apiUrl).pipe(
       tap(response => {
         console.log('Received news data:', response);
       }),
-      map(response => response.articles),
+      map(response => {
+        const articles = response.articles || [];
+        // Store the data in transfer state if we're on the server
+        if (isPlatformServer(this.platformId)) {
+          this.transferState.set(NEWS_KEY, articles);
+        }
+        return articles;
+      }),
       catchError(this.handleError)
     );
   }
 
   getNewsByTopic(topic: string): Observable<NewsArticle[]> {
+    if (!topic) {
+      return of([]);
+    }
+
     console.log('Fetching news for topic:', topic);
     return this.http.get<NewsResponse>(`${this.apiUrl}/topic/${topic}`).pipe(
       tap(response => {
         console.log('Received filtered news data:', response);
       }),
-      map(response => response.articles),
+      map(response => response.articles || []),
       catchError(this.handleError)
     );
   }
@@ -52,6 +84,8 @@ export class NewsService {
     }
     
     console.error('API Error:', errorMessage);
-    throw new Error(errorMessage);
+    
+    // Return an empty array instead of throwing an error
+    return of([]) as any;
   }
 } 
